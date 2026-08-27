2d79bd1356b6ad8d955bb779921c88aaa694282b
astehno <37406637+astehno@users.noreply.github.com>
2026-08-27 09:05:30 -0700

    Update chess_pt_config.js
    added more options

--- chess_pt_config.js
+++ chess_pt_config.js
@@ -32,7 +32,7 @@ const workflowData = [
   {
     id: "s3", name: "3. Probabilistic Sampling", colorVar: "var(--tier-3-color)",
     subs: [
-      { id: "3.1", name: "Deterministic" },
+      { id: "3.1", name: "Deterministic  **" },
       { id: "3.2", name: "PROS - Frequency Based" },
       { id: "3.3", name: "PROS - Response Based" },
       { id: "3.4", name: "LCS" },
@@ -46,8 +46,8 @@ const workflowData = [
     { id: "4.1.1", name: "Levee: Overtopping Rate", colorVar: "var(--tier-4-color)" },
     { id: "4.1.2", name: "Levee: Overtopping Vol", colorVar: "var(--tier-4-color)"},
     { id: "4.1.3", name: "Levee: Runup", colorVar: "var(--tier-4-color)" },
-    { id: "4.1.4", name: "Levee: Cumulative Excess Vol", colorVar: "var(--tier-4-color)", dashed: true },
-    { id: "4.1.5", name: "Levee: Cumulative Excess Work", colorVar: "var(--tier-4-color)", dashed: true },
+    { id: "4.1.4", name: "Levee: Cumulative Excess Vol  **", colorVar: "var(--tier-4-color)", dashed: true },
+    { id: "4.1.5", name: "Levee: Cumulative Excess Work  **", colorVar: "var(--tier-4-color)", dashed: true },
     { id: "4.2.1", name: "Floodwall: Overtopping Rate", colorVar: "var(--tier-4-color)" },
     { id: "4.2.2", name: "Floodwall: Overtopping Vol", colorVar: "var(--tier-4-color)" },
     { id: "4.2.3", name: "Floodwall: Nappe Geometry", colorVar: "var(--tier-4-color)" },
@@ -60,22 +60,26 @@ const workflowData = [
     { id: "4.3.6", name: "Rubble Mound: Overtopping Vol", colorVar: "var(--tier-4-color)" },
     { id: "4.3.7", name: "Rubble Mound: Toe Protection Design", colorVar: "var(--tier-4-color)", dashed: true },
     { id: "4.3.7", name: "Rubble Mound: Revetment Design", colorVar: "var(--tier-4-color)", dashed: true },
+    { id: "4.3.7", name: "Rubble Mound: Low Crested Stability  **", colorVar: "var(--tier-4-color)", dashed: true },
     { id: "4.4.1", name: "NNBF: Wave Attenuation", colorVar: "var(--tier-4-color)", dashed: true },
     { id: "4.4.2", name: "NNBF: Wave Transmission", colorVar: "var(--tier-4-color)", dashed: true },
     { id: "4.4.2", name: "NNBF: Veg Growth/Decay", colorVar: "var(--tier-4-color)", dashed: true },
+    { id: "4.4.3", name: "NNBF: CSHORE-Veg", colorVar: "var(--tier-4-color)", dashed: true },
     { id: "4.5.1", name: "Waves: Wind-Generated", colorVar: "var(--tier-4-color)", dashed: true },
     { id: "4.5.2", name: "Waves: Wave Transformation", colorVar: "var(--tier-4-color)", dashed: true },
     { id: "4.5.3", name: "Waves: Linear Wave Theory", colorVar: "var(--tier-4-color)", dashed: true },
     { id: "4.5.4", name: "Waves: Cnoidal Wave Theory", colorVar: "var(--tier-4-color)", dashed: true },
     { id: "4.5.5", name: "Waves: Fourier Series Wave Theory", colorVar: "var(--tier-4-color)", dashed: true },
     { id: "4.5.6", name: "Waves: Stochastic Peak Value Sampler", colorVar: "var(--tier-4-color)", dashed: true },
+    { id: "4.5.7", name: "Waves: Boussinesq Modeling  **", colorVar: "var(--tier-4-color)", dashed: true },
     { id: "4.6.1", name: "Beaches: Setup", colorVar: "var(--tier-4-color)", dashed: true },
     { id: "4.6.2", name: "Beaches: Irregular Wave Runup", colorVar: "var(--tier-4-color)", dashed: true },
     { id: "4.6.3", name: "Beaches: Longshore Sediment Transport", colorVar: "var(--tier-4-color)", dashed: true },
     { id: "4.6.4", name: "Beaches: Time-Dependent Beach and Dune Erosion", colorVar: "var(--tier-4-color)", dashed: true },
     { id: "4.6.5", name: "Beaches: Nourishment Overfill Ratio and Volume", colorVar: "var(--tier-4-color)", dashed: true },
     { id: "4.6.6", name: "Beaches: Composite Grain-Size Distribution", colorVar: "var(--tier-4-color)", dashed: true },
-    { id: "4.6.7", name: "Beaches: Longshore Transport using CEDRS Stats", colorVar: "var(--tier-4-color)", dashed: true }
+    { id: "4.6.7", name: "Beaches: Longshore Transport using CEDRS Stats", colorVar: "var(--tier-4-color)", dashed: true },
+    { id: "4.6.8", name: "Beaches: CSHORE", colorVar: "var(--tier-4-color)", dashed: true }
     ]
   },
   {
@@ -83,17 +87,18 @@ const workflowData = [
     subs: [
       { id: "5.1", name: "Univariate: JPM Analysis" },
       { id: "5.2", name: "LCS Statistical Analysis" },
-      { id: "5.3", name: "Univariate: PST" },
-      { id: "5.4", name: "Univariate: POT", dashed: true },
-      { id: "5.5", name: "Univariate: MLM Fit", dashed: true },
-      { id: "5.6", name: "Univariate: QQ Optimization", dashed: true },
-      { id: "5.7", name: "Univariate: Bootstrap", dashed: true },
-      { id: "5.8", name: "Multivariate: Correlation", dashed: true },
-      { id: "5.9", name: "Multivariate: Copula", dashed: true },
-      { id: "5.10", name: "Storm Sampling Tool", dashed: true},
-      { id: "5.11", name: "CIRP Integration", dashed: true },
-      { id: "5.12", name: "Water Level Detrending", dashed: true },
-      { id: "5.13", name: "Non-Tidal Residual", dashed: true }
+      { id: "5.3", name: "LCS Cost Analysis  **" },
+      { id: "5.4", name: "Univariate: PST" },
+      { id: "5.5", name: "Univariate: POT", dashed: true },
+      { id: "5.6", name: "Univariate: MLM Fit", dashed: true },
+      { id: "5.7", name: "Univariate: QQ Optimization", dashed: true },
+      { id: "5.8", name: "Univariate: Bootstrap", dashed: true },
+      { id: "5.9", name: "Multivariate: Correlation", dashed: true },
+      { id: "5.10", name: "Multivariate: Copula", dashed: true },
+      { id: "5.11", name: "Storm Sampling Tool", dashed: true},
+      { id: "5.12", name: "CIRP Integration", dashed: true },
+      { id: "5.13", name: "Water Level Detrending", dashed: true },
+      { id: "5.14", name: "Non-Tidal Residual", dashed: true }
     ]
   },
   {
